816f0097079c5abe4984c3ff43e77c4cd6c46ee5
Red <red.sarouf2014@gmail.com>
2023-06-30 10:36:05 -0400

    add place for each property

--- src/component/AddPlayerStat.js
+++ src/component/AddPlayerStat.js
@@ -4,35 +4,35 @@ function AddPlayerStat() {
   return (
     <div className="form">
     <form className="formdata">
-      <input type="text" />
-      <input type="text" /><br/><br/>
-      <input type="text" />
-      <input type="text" /><br/><br/>
-      <input type="text" />
-      <input type="text" /><br/><br/>
-      <input type="text" />
-      <input type="text" /><br/><br/>
-      <input type="text" />
-      <input type="text" /><br/><br/>
-      <input type="text" />
-      <input type="text" /><br/><br/>
-      <input type="text" />
-      <input type="text" /><br/><br/>
-      <input type="text" />
-      <input type="text" /><br/><br/>
-      <input type="text" />
-      <input type="text" /><br/><br/>
-      <input type="text" />
-      <input type="text" /><br/><br/>
-      <input type="text" />
-      <input type="text" /><br/><br/>
-      <input type="text" />
-      <input type="text" /><br/><br/>
-      <input type="text" />
-      <input type="text" /><br/><br/>
-      <input type="text" />
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
+      <input type="text" placeholder="Player Name"/>
+      <input type="text" placeholder="Player Name"/><br/><br/>
+      <input type="text" placeholder="Player Name"/>
+      <input type="text" placeholder="Player Name"/><br/><br/>
+      <input type="text" placeholder="Player Name"/>
+      <input type="text" placeholder="Player Name"/><br/><br/>
+      <input type="text" placeholder="Player Name"/>
+      <input type="text" placeholder="Player Name"/><br/><br/>
+      <input type="text" placeholder="Player Name"/>
+      <input type="text" placeholder="Player Name"/><br/><br/>
+      <input type="text" placeholder="Player Name"/>
+      <input type="text" placeholder="Player Name"/><br/><br/>
+      <input type="text" placeholder="Player Name"/>
+      <input type="text" placeholder="Player Name"/><br/><br/>
+      <input type="text" placeholder="Player Name"/>
+      <input type="text" placeholder="Player Name"/><br/><br/>
+      <input type="text" placeholder="Player Name"/>
+      <input type="text" placeholder="Player Name"/><br/><br/>
+      <input type="text" placeholder="Player Name"/>
+      <input type="text" placeholder="Player Name"/><br/><br/>
+      <input type="text" placeholder="Player Name"/>
+      <input type="text" placeholder="Player Name"/><br/><br/>
+      <input type="text" placeholder="Player Name"/>
+      <input type="text" placeholder="Player Name"/><br/><br/>
+      <input type="text" placeholder="Player Name"/>
+      <input type="text" placeholder="Player Name"/><br/><br/>
+      <input type="text" placeholder="Player Name"/>
+      <input type="text" placeholder="Player Name"/><br/><br/>
+      <input type="text" placeholder="Player Name"/><br/><br/>
       <button id="submitbtn" >Submit</button>
     </form>
     </div>
