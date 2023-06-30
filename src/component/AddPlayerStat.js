67fb45c51475df5eada9307b25cd133ca4bafe6c
Red <red.sarouf2014@gmail.com>
2023-06-30 10:33:23 -0400

    add style to form add palyer stats

--- src/component/AddPlayerStat.js
+++ src/component/AddPlayerStat.js
@@ -2,38 +2,40 @@ import React from "react";
 
 function AddPlayerStat() {
   return (
+    <div className="form">
     <form className="formdata">
+      <input type="text" />
+      <input type="text" /><br/><br/>
+      <input type="text" />
+      <input type="text" /><br/><br/>
+      <input type="text" />
+      <input type="text" /><br/><br/>
+      <input type="text" />
+      <input type="text" /><br/><br/>
+      <input type="text" />
+      <input type="text" /><br/><br/>
+      <input type="text" />
+      <input type="text" /><br/><br/>
+      <input type="text" />
+      <input type="text" /><br/><br/>
+      <input type="text" />
+      <input type="text" /><br/><br/>
+      <input type="text" />
+      <input type="text" /><br/><br/>
+      <input type="text" />
+      <input type="text" /><br/><br/>
+      <input type="text" />
+      <input type="text" /><br/><br/>
+      <input type="text" />
+      <input type="text" /><br/><br/>
+      <input type="text" />
+      <input type="text" /><br/><br/>
+      <input type="text" />
       <input type="text" /><br/><br/>
       <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <input type="text" /><br/><br/>
-      <button>Submit</button>
+      <button id="submitbtn" >Submit</button>
     </form>
+    </div>
   );
 }
 
