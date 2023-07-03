2b5dc5120d0ed9c9ff147f927f475de2079ad1d3
Red <red.sarouf2014@gmail.com>
2023-07-03 14:40:01 -0400

    refactor filter method

--- src/component/Home.js
+++ src/component/Home.js
@@ -76,7 +76,7 @@ function Home() {
         <tbody>
           {users
             .filter(user => {
-              return (search?.toLocaleLowerCase() || search?.toLocaleUpperCase() === "") ? user : user.player_name?.toLocaleLowerCase().includes(search) || user.player_name?.toLocaleLowerCase().includes(search);
+              return (search?.toLowerCase() || search?.toUpperCase() === "") ? user : user.player_name?.toLowerCase().includes(search) || user.player_name?.toLowerCase().includes(search);
             })
             .map((user) => (
               <tr key={user.id}>
